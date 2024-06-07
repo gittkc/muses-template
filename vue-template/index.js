664fa4a99fe97f275ce19cf2ec82ee748900613b
gittkc <166570930+gittkc@users.noreply.github.com>
2024-06-07 11:32:37 +0900

    コメント追加

--- vue-template/index.js
+++ vue-template/index.js
@@ -5,15 +5,22 @@ PetiteVue.createApp({
   unread: '',
 
   async init() {
+    //ログイン中のユーザーを取得
     const username = sessionStorage.username;
+    //ユーザー名がfalse?ログインしていない場合
     if (!username) {
+      //ウィンドウアラート表示
       window.alert('ログインしてください');
+      //ログイン画面へ遷移
       location.href = 'login.html';
     }
     this.username = username;
 
+    //data.jsonの読み込み
     const res = await fetch('data.json');
     const obj = await res.json();
+    //読み込んだdata.jsonのListの要素数をspanタグclass="unread"のテキストに表示
+    //新着情報に「未読掲示が3件あります！」の表示
     this.unread = obj.list.length;
   }
 }).mount();
--- vue-template/menu.js
+++ vue-template/menu.js
@@ -5,18 +5,25 @@ PetiteVue.createApp({
   unread: '',
   data: [],
   async init() {
+    //ログイン中のユーザー名を取得
     const username = sessionStorage.username;
+    //ユーザー名がfalse?ログインしていない場合
     if (!username) {
+      //ウィンドウアラート表示
       window.alert('ログインしてください');
+      //ログイン画面へ遷移
       location.href = 'login.html';
     }
     this.username = username;
 
+    //data.jsonの読み込み
     const res = await fetch('data.json');
     const obj = await res.json();
     this.data = obj.list;
     console.log(this.data);
 
+    //新着情報に「未読掲示が○件あります！」の表示
+    //件数を取得
     this.unread = this.data.length;
   }
 }).mount();
